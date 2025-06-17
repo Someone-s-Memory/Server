ff1d3e26a148237494623c54baac8c6be66ffda5
minsung gwak <131960511+opinionist@users.noreply.github.com>
2025-06-17 15:11:07 +0900

    spring SecurityConfig 수정

--- src/main/java/example/soloproject/global/config/SecurityConfig.java
+++ src/main/java/example/soloproject/global/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package example.soloproject.global.config;
 
 import example.soloproject.global.jwt.CustomAccessDeniedHandler;
 import example.soloproject.global.jwt.CustomAuthenticationEntryPoint;
+import example.soloproject.global.jwt.JwtAuthenticationFilter;
+import example.soloproject.global.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,10 +11,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
@@ -22,17 +27,18 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션을 사용하지 않음
 
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()) // 모든 요청을 허용
+                        .anyRequest().permitAll()) // 모든 요청을 허용 -> 나중에 변경할 거
 
-                .formLogin(form -> form.disable()) // 폼 로그인 비활성화
+                .formLogin(form -> form.disable()) // 폼 로그인 비활성화 -> 나중에 변경할 거
 
-                .httpBasic(http -> http.disable())
+                .httpBasic(http -> http.disable()) // HTTP Basic 인증 비활성화 -> 나중에 변경할 거
 
-                .exceptionHandling(exception -> exception // 나중에 custom 할 거임
+                .exceptionHandling(exception -> exception
                         .authenticationEntryPoint(new CustomAuthenticationEntryPoint())// 인증되지 않은 경우
                         .accessDeniedHandler(new CustomAccessDeniedHandler())// 권한이 없는 경우
                 )
-        ; // HTTP Basic 인증 비활성화
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+        ;
 
         return httpSecurity.build();
     }
