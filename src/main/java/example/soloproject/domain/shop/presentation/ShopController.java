8fdc33a91f7a1d3817803bab589080370109e4d5
minsung gwak <131960511+opinionist@users.noreply.github.com>
2025-08-21 20:04:14 +0900

    Shop Get 생성 준비

--- src/main/java/example/soloproject/domain/shop/presentation/ShopController.java
+++ src/main/java/example/soloproject/domain/shop/presentation/ShopController.java
@@ -1,6 +1,8 @@
 package example.soloproject.domain.shop.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ShopController {
 
+    @GetMapping("/shop")
+    public ResponseEntity<?> getShop() {
+        return ResponseEntity.ok("상점 정보가 성공적으로 조회되었습니다.");
+    }
 }
--- src/main/java/example/soloproject/domain/shop/service/impl/ShopServiceImpl.java
+++ src/main/java/example/soloproject/domain/shop/service/impl/ShopServiceImpl.java
@@ -1,8 +1,19 @@
 package example.soloproject.domain.shop.service.impl;
 
+import example.soloproject.domain.shop.repository.ShopRepository;
 import example.soloproject.domain.shop.service.ShopService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+
+
 @Service
+@RequiredArgsConstructor
 public class ShopServiceImpl implements ShopService {
+    private final Logger logger = LoggerFactory.getLogger(ShopServiceImpl.class);
+    private final ShopRepository shopRepository;
+
+
 }
